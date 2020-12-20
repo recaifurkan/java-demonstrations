3c9b6d09fa517d4e7cda9977ba479eb06047f4c2
Recai Furkan Bostancı <30552187+recaifurkan@users.noreply.github.com>
2020-12-21 00:03:16 +0300

    the original was kept

---
+++ java-multithreading/src/main/java/com/ufukuzun/multithreading/chapter1/Application1.java
@@ -0,0 +1,25 @@
+package com.ufukuzun.multithreading.chapter1;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+        Runner runner1 = new Runner();
+        Runner runner2 = new Runner();
+
+        runner1.start();
+        runner2.start();
+        System.out.println("çıktı");
+    }
+
+    static class Runner extends Thread {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("Merhaba " + i + " - " + this.getName());
+            }
+        }
+
+    }
+
+}
---
+++ java-multithreading/src/main/java/com/ufukuzun/multithreading/chapter25/Receiver.java
@@ -0,0 +1,30 @@
+package com.ufukuzun.multithreading.chapter25;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Receiver implements Runnable {
+    private Data load;
+
+    public Receiver(Data load) {
+        this.load = load;
+    }
+
+    // standard constructors
+ 
+    public void run() {
+        for(String receivedMessage = load.receive();
+          !"End".equals(receivedMessage);
+          receivedMessage = load.receive()) {
+            
+            System.out.println(receivedMessage);
+
+            // ...
+            try {
+                Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 5000));
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                System.out.println("Thread interrupted");
+            }
+        }
+    }
+}
